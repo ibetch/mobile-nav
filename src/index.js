874d3b71b7072e3e53aa4a88fb0dfd1ad4bd1565
K. Tugangui <ktugangui@gmail.com>
2019-05-13 16:29:30 -0700

    feat(view all links): create view all links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-document.getElementById("app").innerHTML = `
+document.getElementById('app').innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+/**
+ * target all child lists
+ */
+var childLists = document.querySelectorAll('ul > li > ul');
+
+/**
+ * create view all link for all parent list
+ */
+function createViewAllLink(list) {
+  list.forEach(function(item) {
+    // get parent item
+    var parentAnchor = item.parentNode.children[0];
+
+    // create clone
+    var clone = parentAnchor.cloneNode(true);
+
+    // update clone text
+    var cloneText = 'View All ' + parentAnchor.text;
+    clone.textContent = cloneText;
+
+    // remove href value from parent items
+    parentAnchor.setAttribute('href', '');
+
+    // create viewAllLink
+    var viewAllLink = document.createElement('li');
+    viewAllLink.appendChild(clone);
+
+    // insert viewAllLink as first item
+    item.insertBefore(viewAllLink, item.firstChild);
+
+    return;
+  });
+  return;
+}
+
+createViewAllLink(childLists);
